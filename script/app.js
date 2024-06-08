b1da29c726216135de8d9d8518eac3f893aa1538
Shyshkanova2005 <ivanovasasha883@gmail.com>
2024-06-08 17:41:52 +0300

    feat: display menu (#4)

--- script/app.js
+++ script/app.js
@@ -5,3 +5,12 @@ b.createBoard()
 b.setMines()
 b.setDigits()
 b.displayBoard()
+
+const escapePressed = () => {   
+    const menu = document.querySelector('.menu')
+    menu.classList.toggle('display')
+    const board = document.querySelector('.board')
+    board.classList.toggle('hide')
+}
+
+document.addEventListener('keydown', escapePressed)
