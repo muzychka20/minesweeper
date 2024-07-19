9cc34af5b46d4ff012b3a8755b5bc9c5997190d2
KyryloMuzychka <kyryll2016@gmail.com>
2024-07-19 16:15:18 +0300

    fix: right click mobile

--- script/classes/board/BoardInterface.js
+++ script/classes/board/BoardInterface.js
@@ -1,182 +1,194 @@
-import { Board } from './Board.js'
-import { OpenCellCommand } from '../cell/OpenCellCommand.js'
-import { NumberCellCommand } from '../cell/NumberCellCommand.js'
-import { ClosedCellCommand } from '../cell/ClosedCellCommand.js'
-import { FlaggedCellCommand } from '../cell/FlaggedCellCommand.js'
-import { MineCellCommand } from '../cell/MineCellCommand.js'
-import { RemoveClassCommand } from '../cell/RemoveClassCommand.js';
+import { Board } from "./Board.js";
+import { OpenCellCommand } from "../cell/OpenCellCommand.js";
+import { NumberCellCommand } from "../cell/NumberCellCommand.js";
+import { ClosedCellCommand } from "../cell/ClosedCellCommand.js";
+import { FlaggedCellCommand } from "../cell/FlaggedCellCommand.js";
+import { MineCellCommand } from "../cell/MineCellCommand.js";
+import { RemoveClassCommand } from "../cell/RemoveClassCommand.js";
 
 export class BoardInterface {
-    board = null
-    timer = null
-    counterMines = null
+  board = null;
+  timer = null;
+  counterMines = null;
 
-    constructor(timer, counterMines) {
-        let width = document.getElementById('width').value
-        let height = document.getElementById('height').value
-        let mines = document.getElementById('mines').value
+  constructor(timer, counterMines) {
+    let width = document.getElementById("width").value;
+    let height = document.getElementById("height").value;
+    let mines = document.getElementById("mines").value;
 
-        if (width * height <= mines) {
-            alert("Enter the correct amount of mines!")
-        } else {
-            this.board = new Board(width, height, mines)
-            this.board.createBoard()
-            this.setBoard()            
-            this.timer = timer
-            this.counterMines = counterMines
-            this.counterMines.setMaxMines(mines)
-            this.counterMines.changeInterface()
-        }
+    if (width * height <= mines) {
+      alert("Enter the correct amount of mines!");
+    } else {
+      this.board = new Board(width, height, mines);
+      this.board.createBoard();
+      this.setBoard();
+      this.timer = timer;
+      this.counterMines = counterMines;
+      this.counterMines.setMaxMines(mines);
+      this.counterMines.changeInterface();
+    }
+  }
+
+  setBoard() {
+    let boardContainer = document.querySelector(".board");
+    boardContainer.innerHTML = "";
+    for (let i = 0; i < this.board.getHeight(); i++) {
+      let row = document.createElement("div");
+      row.classList.add("row");
+      for (let j = 0; j < this.board.getWidth(); j++) {
+        let cellBlock = document.createElement("div");
+        cellBlock.setAttribute("id", "cell");
+        let closedCellCommand = new ClosedCellCommand();
+        closedCellCommand.execute(cellBlock);
+
+        let longPressTimer;
+        const longPressDuration = 500; // Время долгого нажатия в миллисекундах
+
+        const startLongPressTimer = (event) => {
+          event.preventDefault();
+          longPressTimer = setTimeout(() => {
+            this.rightClickOnCell(i, j);
+          }, longPressDuration);
+        };
+
+        const cancelLongPressTimer = (event) => {
+          clearTimeout(longPressTimer);
+          if (event.type === "mouseup" && event.button === 0) {
+            this.leftClickOnCell(i, j);
+          }
+        };
+
+        cellBlock.addEventListener("mousedown", (event) => {
+          if (event.button === 0) {
+            startLongPressTimer(event);
+          } else if (event.button === 2) {
+            this.rightClickOnCell(i, j);
+          }
+        });
+
+        cellBlock.addEventListener("mouseup", cancelLongPressTimer);
+        cellBlock.addEventListener("mouseleave", cancelLongPressTimer);
+
+        cellBlock.addEventListener("touchstart", startLongPressTimer);
+        cellBlock.addEventListener("touchend", cancelLongPressTimer);
+        cellBlock.addEventListener("touchmove", cancelLongPressTimer);
+
+        row.appendChild(cellBlock);
+      }
+      boardContainer.appendChild(row);
+    }
+  }
+
+  getCell(i, j) {
+    return document.querySelector(".board").childNodes[i].childNodes[j];
+  }
+
+  leftClickOnCell(i, j) {
+    if (this.board.isFirstClick()) {
+      this.board.setMines(i, j);
+      this.board.setDigits();
+      this.board.makeFirstClick();
+      this.timer.startTimer();
     }
 
-    setBoard() {        
-        let boardContainer = document.querySelector('.board')
-        boardContainer.innerHTML = ""
-        for (let i = 0; i < this.board.getHeight(); i++) {
-            let row = document.createElement('div')
-            row.classList.add('row')
-            for (let j = 0; j < this.board.getWidth(); j++) {
-                let cellBlock = document.createElement('div')
-                cellBlock.setAttribute('id', 'cell')
-                let closedCellCommand = new ClosedCellCommand()
-                closedCellCommand.execute(cellBlock)                
-                let longPressTimer
-                const longPressDuration = 25
-                cellBlock.addEventListener('mousedown', event => {
-                    if (event.button === 0) {                        
-                        longPressTimer = setTimeout(() => {
-                            this.rightClickOnCell(i, j);
-                        }, longPressDuration);
-                    } else if (event.button === 2) {
-                        this.rightClickOnCell(i, j);
-                    }
-                })
-                cellBlock.addEventListener('mouseup', event => {
-                    if (event.button === 0) {                        
-                        clearTimeout(longPressTimer);
-                        this.leftClickOnCell(i, j);
-                    }
-                })
-                row.appendChild(cellBlock)
-            }
-            boardContainer.appendChild(row)
-        }
+    if (this.board.isFlagged(i, j)) {
+      this.board.deleteFlag(i, j);
+      let removeClassCommand = new RemoveClassCommand("flagged_yellow");
+      removeClassCommand.execute(this.getCell(i, j));
+      this.counterMines.removeMine();
     }
 
-    getCell(i, j) {
-        return document.querySelector('.board').childNodes[i].childNodes[j]
+    if (this.board.isMine(i, j)) {
+      this.displayMines();
+      setTimeout(() => {
+        alert("You lose!");
+      }, 100);
+      this.removeEventListeners();
+      this.timer.stopTimer();
+      return;
     }
+    this.board.openCell(i, j);
+    this.displayOpenCells();
 
-    leftClickOnCell(i, j) {        
-        if (this.board.isFirstClick()) {
-            this.board.setMines(i, j)
-            this.board.setDigits()
-            this.board.makeFirstClick()
-            this.timer.startTimer()
-        } 
+    if (this.board.checkVictory()) {
+      this.displayVictoryFlags();
+      setTimeout(() => {
+        alert("You win!");
+      }, 100);
+      this.removeEventListeners();
+      this.timer.stopTimer();
+    }
+  }
 
-        if (this.board.isFlagged(i, j)) {
-            this.board.deleteFlag(i, j)
-            let removeClassCommand = new RemoveClassCommand('flagged_yellow')
-            removeClassCommand.execute(this.getCell(i, j))
-            this.counterMines.removeMine()
+  rightClickOnCell(i, j) {
+    if (!this.board.isOpen(i, j)) {
+      if (this.board.isFlagged(i, j)) {
+        this.board.deleteFlag(i, j);
+        this.counterMines.removeMine();
+        let closedCell = new ClosedCellCommand("flagged_yellow");
+        closedCell.execute(this.getCell(i, j));
+      } else {
+        if (this.counterMines.addMine()) {
+          this.board.setFlag(i, j);
+          let add = new FlaggedCellCommand("yellow");
+          add.execute(this.getCell(i, j));
         }
-       
+      }
+    }
+  }
+
+  removeEventListeners() {
+    for (let i = 0; i < this.board.getHeight(); i++) {
+      for (let j = 0; j < this.board.getWidth(); j++) {
+        let cell = this.getCell(i, j);
+        let newCell = cell.cloneNode(true);
+        cell.parentNode.replaceChild(newCell, cell);
+      }
+    }
+  }
+
+  displayMines() {
+    for (let i = 0; i < this.board.getHeight(); i++) {
+      for (let j = 0; j < this.board.getWidth(); j++) {
+        if (this.board.isMine(i, j) && this.board.isFlagged(i, j)) {
+          let flaggedCellCommand = new FlaggedCellCommand("green");
+          flaggedCellCommand.execute(this.getCell(i, j));
+        } else if (this.board.isFlagged(i, j)) {
+          let flaggedCellCommand = new FlaggedCellCommand("red");
+          flaggedCellCommand.execute(this.getCell(i, j));
+        } else if (this.board.isMine(i, j)) {
+          let mineCellCommand = new MineCellCommand();
+          mineCellCommand.execute(this.getCell(i, j));
+        }
+      }
+    }
+  }
+
+  displayOpenCells() {
+    let openCellCommand = new OpenCellCommand();
+    let removeClosed = new RemoveClassCommand("closed");
+    for (let i = 0; i < this.board.getWidth(); i++) {
+      for (let j = 0; j < this.board.getHeight(); j++) {
+        if (this.board.isOpen(i, j)) {
+          let cell = this.getCell(i, j);
+          openCellCommand.execute(cell);
+          let quantity = this.board.getQuantity(i, j);
+          let numberCellCommand = new NumberCellCommand(quantity);
+          numberCellCommand.execute(cell);
+          removeClosed.execute(cell);
+        }
+      }
+    }
+  }
+
+  displayVictoryFlags() {
+    for (let i = 0; i < this.board.getHeight(); i++) {
+      for (let j = 0; j < this.board.getWidth(); j++) {
         if (this.board.isMine(i, j)) {
-            this.displayMines()
-            setTimeout(() => {
-                alert('You lose!')
-            }, 100)
-            this.removeEventListeners()
-            this.timer.stopTimer()
-            return
+          let flaggedCellCommand = new FlaggedCellCommand("green");
+          flaggedCellCommand.execute(this.getCell(i, j));
         }
-        this.board.openCell(i, j)
-        this.displayOpenCells()
-        
-        if (this.board.checkVictory()) {
-            this.displayVictoryFlags()
-            setTimeout(() => {
-                alert('You win!')
-            }, 100)
-            this.removeEventListeners()
-            this.timer.stopTimer()
-        }
+      }
     }
-
-    rightClickOnCell(i, j) {
-        if (!this.board.isOpen(i, j)) {
-            if (this.board.isFlagged(i, j)) {
-                this.board.deleteFlag(i, j)
-                this.counterMines.removeMine()
-                let closedCell = new ClosedCellCommand('flagged_yellow')
-                closedCell.execute(this.getCell(i, j))
-            }
-            else {
-                if (this.counterMines.addMine()) {
-                    this.board.setFlag(i, j)          
-                    let add = new FlaggedCellCommand('yellow')
-                    add.execute(this.getCell(i, j))
-                }
-            }
-        }
-    }
-
-    removeEventListeners() {
-        for (let i = 0; i < this.board.getHeight(); i++) {
-            for (let j = 0; j < this.board.getWidth(); j++) {
-                let cell = this.getCell(i, j)
-                let newCell = cell.cloneNode(true)
-                cell.parentNode.replaceChild(newCell, cell)
-            }
-        }
-    }
-
-    displayMines() {           
-        for (let i = 0; i < this.board.getHeight(); i++) {
-            for (let j = 0; j < this.board.getWidth(); j++) {
-                if (this.board.isMine(i ,j) && this.board.isFlagged(i, j)) {                    
-                    let flaggedCellCommand = new FlaggedCellCommand('green')
-                    flaggedCellCommand.execute(this.getCell(i, j))
-                }
-                else if (this.board.isFlagged(i, j)) {
-                    let flaggedCellCommand = new FlaggedCellCommand('red')
-                    flaggedCellCommand.execute(this.getCell(i, j))
-                }                
-                else if (this.board.isMine(i ,j)) {
-                    let mineCellCommand = new MineCellCommand()
-                    mineCellCommand.execute(this.getCell(i, j))
-                }
-            }
-        }        
-    }
-
-    displayOpenCells() {        
-        let openCellCommand = new OpenCellCommand()
-        let removeClosed = new RemoveClassCommand('closed')
-        for (let i = 0; i < this.board.getWidth(); i++) {            
-            for (let j = 0; j < this.board.getHeight(); j++) {                  
-                if (this.board.isOpen(i, j)) {
-                    let cell = this.getCell(i, j)                    
-                    openCellCommand.execute(cell)
-                    let quantity = this.board.getQuantity(i, j)
-                    let numberCellCommand = new NumberCellCommand(quantity)
-                    numberCellCommand.execute(cell)
-                    removeClosed.execute(cell)
-                }    
-            }
-        }
-    }
-
-    displayVictoryFlags() {
-        for (let i = 0; i < this.board.getHeight(); i++) {
-            for (let j = 0; j < this.board.getWidth(); j++) {
-                if (this.board.isMine(i, j)) {
-                    let flaggedCellCommand = new FlaggedCellCommand('green')
-                    flaggedCellCommand.execute(this.getCell(i, j))
-                }
-            }
-        }
-    }
-
+  }
 }
